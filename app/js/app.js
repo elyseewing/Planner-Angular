bec0e8259e6e3965282e2c58c5fa4e13915263cc
Elyse Dougherty <elysedougherty@gmail.com>
2012-11-11 10:17:46 -0500

    Initial commit

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,12 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/signup', {templateUrl: 'partials/signup.html', controller: SignupCtrl});
+    $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: LoginCtrl});
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
+    $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: MyCtrl2});
+    $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: MyCtrl2});
+    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: MyCtrl2});
     $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,11 +2,14 @@
 
 /* Controllers */
 
+function SignupCtrl() {}
+SignupCtrl.$inject = [];
+
+function LoginCtrl() {}
+LoginCtrl.$inject = [];
 
 function MyCtrl1() {}
 MyCtrl1.$inject = [];
 
-
-function MyCtrl2() {
-}
+function MyCtrl2() {}
 MyCtrl2.$inject = [];
